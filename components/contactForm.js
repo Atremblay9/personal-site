dd098b18d60eacb1b86450bc496af3cad9d5c315
Atremblay9 <atremblay9@nait.ca>
2025-01-05 20:37:38 -0700

    form

--- components/contactForm.js
+++ components/contactForm.js
@@ -1,6 +1,8 @@
+
+
 import Grid from '@mui/material/Grid2';
  
-export default function ContactForm() {
+export function ContactForm() {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -13,21 +15,21 @@ export default function ContactForm() {
   };
  
   return (
-    <form id="contact-form" name="contact" onSubmit={handleFormSubmit} >
+    <form name="contact" onSubmit={handleFormSubmit} >
                 <Grid container spacing={2} style={{padding: '1rem 0'}}>
                     <Grid size={12}>
-                    <label for="user-email" class="form-label">Your Email</label>
+                    <label for="user-email" >Your Email</label>
                     </Grid>
                     <Grid size={12}>
-                    <input type="email" class="form-control" id="user-email" name="userEmail" placeholder="Enter your email" required style={{width: '50%'}} />
+                    <input type="email"  id="user-email" name="userEmail" placeholder="Enter your email" required style={{width: '50%'}} />
                     </Grid>
                 </Grid>
                 <Grid container spacing={2} style={{padding: '1rem 0'}}>
                     <Grid size={12}>
-                    <label for="user-message" class="form-label">Your Message</label>
+                    <label for="user-message" >Your Message</label>
                     </Grid>
                     <Grid size={12}>
-                    <textarea class="form-control" id="user-message" name="userMessage" rows="6" required  style={{width: '100%'}} ></textarea>
+                    <textarea id="user-message" name="userMessage" rows="6" required  style={{width: '100%'}} ></textarea>
                     </Grid>
                 </Grid>
                 <button type="submit" className="submit-button">Submit</button>
