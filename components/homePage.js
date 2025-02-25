53ad60cac5da7985df5f408c0e210fe6ab0b37e8
Atremblay9 <atremblay9@nait.ca>
2025-02-25 09:48:29 -0700

    Added headshot

--- components/homePage.js
+++ components/homePage.js
@@ -21,7 +21,7 @@ export default function HomePage() {
                 <h2 style={{ margin: 0 }}>Software Developer</h2>
             </Grid>
             <Grid size={6}>
-                
+                <img src="/img/headshot.jpg" alt="Amélie Tremblay" style={{ width: '100%', height: '100%',borderRadius:'50%', objectFit: 'cover' }} />
             </Grid>
             
         </Grid>
